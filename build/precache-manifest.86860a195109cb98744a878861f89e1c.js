944fcddba8c03171c86ec11f17d5397d0e86e68b
Chetna <chetnavats11@gmail.com>
2021-07-04 16:51:05 +0530

    done

---
+++ build/precache-manifest.86860a195109cb98744a878861f89e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3137d92615a70b16fa2b7acd2f2a35f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b341a1ba3865c36216b3",
+    "url": "/static/css/main.b8f83645.chunk.css"
+  },
+  {
+    "revision": "2495795e7abdf9b8ee2e",
+    "url": "/static/js/2.2b34988c.chunk.js"
+  },
+  {
+    "revision": "4ed2d4f00cf8324f872f01907affe619",
+    "url": "/static/js/2.2b34988c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b341a1ba3865c36216b3",
+    "url": "/static/js/main.86533979.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
